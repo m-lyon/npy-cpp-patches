be8c5229871cbb7a88aa7a7261e3ec25c4a4576c
Matt <32886639+m-lyon@users.noreply.github.com>
2022-06-17 14:54:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ ext_modules = [
 ]
 
 
-version = '1.0.4'
+version = '1.0.5'
 this_dir = path.abspath(path.dirname(__file__))
 with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
